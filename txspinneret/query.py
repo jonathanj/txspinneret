a7395c7dd5a3fa2220109a76948b1ac746186d64
Jonathan Jacobs <jonathan@jsphere.com>
2014-05-09 21:48:32 +0200

    Goodbye broken Python3 support!

--- txspinneret/query.py
+++ txspinneret/query.py
@@ -27,13 +27,6 @@ from txspinneret.util import maybe, UTC
 
 
 
-try:
-    unicode
-except NameError:
-    unicode = str
-
-
-
 def _isSequenceTypeNotText(x):
     """
     Is this a ``sequence`` type that isn't also a ``string`` type?
